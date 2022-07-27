82e94fc87fd7808362fa54815718bbfa50963bc5
nguyen van vu <atrox427@gmail.com>
2022-07-27 07:48:32 +0700

    Add project files.

---
+++ WebAppTinhVanCat_aspnetcore/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebAppTinhVanCat_aspnetcore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
