52d6f91dffeb6672532b177789ad334a2d590f93
nguyen van vu <atrox427@gmail.com>
2022-09-04 21:59:09 +0700

    update manage database

---
+++ WebAppTinhVanCat_aspnetcore/Areas/Database/Controllers/DbManageController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAppTinhVanCat_aspnetcore.Models;
+
+namespace WebAppTinhVanCat_aspnetcore.Areas.Database.Controllers
+{
+    [AllowAnonymous]
+    [Area("Database")]
+    [Route("/database-manage/[action]")]
+    
+    public class DbManageController : Controller
+    {
+        private readonly AppDbContext _dbContext;
+
+        public DbManageController(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+
+
+        //get : /database-manage/index
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult DeleteDb()
+        {
+            return View();
+        }
+
+        [TempData]
+        public string StatusMessage { get; set; }
+
+        [HttpPost]
+        public async Task<IActionResult> DeleteDbAsync()
+        {
+            var success = await _dbContext.Database.EnsureDeletedAsync();
+
+            StatusMessage = success ? "Xóa Database thành công !" : "Không xóa được !";
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> MigrateAsync()
+        {
+            await _dbContext.Database.MigrateAsync();
+            StatusMessage = "Tạo Database thành công !";
+
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
